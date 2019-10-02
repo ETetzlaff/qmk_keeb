91a6b9e2cc3ea177bc6b26bc435094d2f82ffc96
Evan Tetzlaff <evan.tetzlaff@ooda-health.com>
2019-10-02 09:36:52 -0600

    check for qmk_firmware repo

--- dz60.sh
+++ dz60.sh
@@ -1,3 +1,6 @@
+if [[ ! -e qmk_firmware ]]; then
+    git clone git@github.com:qmk/qmk_firmware.git
+fi
 rm -rf qmk_firmware/keyboards/dz60/keymaps/reuhssurance
 cp -rf dz60/reuhssurance qmk_firmware/keyboards/dz60/keymaps/reuhssurance
 cd qmk_firmware
